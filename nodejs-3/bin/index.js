8823ab43d9c4a09edb8417271d082944231c5a56
Илья Колмаков <ilakolmakov@MacBook-Pro-Ila.local>
2023-01-25 22:32:45 +0500

    nodejs-3 add tree files function

--- nodejs-3/bin/index.js
+++ nodejs-3/bin/index.js
@@ -1,22 +1,89 @@
 #!/usr/bin/env node
 
-console.log("123");
+const path = require("path");
+const { readdir, stat } = require("node:fs/promises");
 
-const argList = process.argv.splice(2);
-console.log(argList);
-
+// Объект для параметров командной строки
 const argObj = {};
+// Путь до директории
+argObj.pathDir = process.argv[2];
 
-argList.forEach((arg) => {
-  const argEntity = arg.split("=");
-  if (
-    (/--/i.test(argEntity[0]) && argEntity[0].slice(2) === "depth") ||
-    (/-/i.test(argEntity[0]) && argEntity[0].slice(1) === "d")
-  ) {
-    argObj.depth = Number(argEntity[1]);
-  } else {
-    console.log("Error");
+getArgList(process.argv.splice(3));
+
+function getArgList(argvs) {
+  argvs.forEach((arg) => {
+    const argEntity = arg.split("=");
+    if (
+      (/--/i.test(argEntity[0]) && argEntity[0].slice(2) === "depth") ||
+      (/-/i.test(argEntity[0]) && argEntity[0].slice(1) === "d")
+    ) {
+      argObj.depth = Number(argEntity[1]);
+    } else {
+      console.error("Error");
+    }
+  });
+}
+
+function printTree(data, level = 0) {
+
+  if (level > argObj.depth) {
+    return;
   }
-});
 
-console.log(argObj);
+  console.log(
+    `${!level ? '' : '|  '.repeat(level - 1) + (data.items ? '├──' : '└──')}${
+      data.name
+    }`
+  );
+
+  data.items?.forEach((item) => {
+    printTree(item, level + 1);
+  });
+}
+
+const info = {
+  directories: 0,
+  files: 0,
+};
+
+const readDirFiles = async (currPath, dataFile) => {
+  try {
+    const files = await readdir(currPath);
+
+    for (const file of files) {
+      const fileItem = path.resolve(currPath, file);
+      const fileStat = await stat(fileItem);
+      if (fileStat.isFile()) {
+        dataFile.push({name: file});
+        info.files++;
+      } else if (fileStat.isDirectory()) {
+        dataFile.push({ name: file, items: [] });
+        info.directories++;
+        readDirFiles(fileItem, dataFile[dataFile.length-1].items);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const currentPath = path.resolve(__dirname, argObj.pathDir);
+const data = {};
+data.name = path.basename(currentPath);
+data.items = [];
+
+const init = async () => {
+  await readDirFiles(currentPath, data.items);
+  // Не работает await на вложенных массивах
+  setTimeout(() => {
+    printTree(data);
+    console.log(`${info.directories} director${info.directories > 1 ? "ies" : "y"}, ${info.files} file${info.files > 1 ? "s" : ""}`);
+  });
+  // await printTree(data);
+}
+
+init();
+
+module.exports = init;
+
+
